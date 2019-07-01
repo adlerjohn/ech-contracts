8c03701e0bcd2f8f6f5e8ac360499f6ebdbf6be9
John Adler <john.adler@protonmail.ch>
2019-07-01 12:01:47 -0400

    Implement finalization and cleanups.

--- contracts/DepositContract.sol
+++ contracts/DepositContract.sol
@@ -7,23 +7,31 @@ import "openzeppelin-solidity/contracts/math/SafeMath.sol";
 import "contracts/libs/MerkleProof256.sol";
 
 contract DepositContract  {
-    using SafeMath for uint64;
+    using SafeMath for uint256;
+
+    struct BlockCommitment {
+        bytes32 hash;
+        uint256 ethereumHeight;
+        address payable from;
+        bool isNotFinalized;
+    }
 
     struct BlockHeader {
+        // TODO maybe store a Merkle accumulator of prev blocks?
         bytes32 prev;
-        uint64 height;
+        uint256 height;
         bytes32 stateRoot;
         bytes32 txRoot;
     }
 
     struct ExitWitness {
-        bytes32 witness_r;
-        bytes32 witness_s;
-        byte witness_v;
+        bytes32 r;
+        bytes32 s;
+        byte v;
     }
 
     struct ExitTransaction {
-        address from;
+        address payable from;
         uint256 amount;
         address color;
         uint64 nonce;
@@ -32,51 +40,71 @@ contract DepositContract  {
 
     uint256 constant public BOND_SIZE = 1 ether;
     uint256 constant public FINALIZATION_DELAY = 6000; // ~ 1 day
-    address constant public ZERO_ADDRESS = 0x0000000000000000000000000000000000000000;
+    address constant public ZERO_ADDRESS = address(0);
 
     event Deposit(address from, uint256 amount, address color, uint256 nonce);
 
-    bytes32[] public s_blockHashes;
-    uint64 public s_tipHeight = 0;
-    uint256 public s_tipEthereumHeight = 0;
+    BlockCommitment[] public s_commitments;
+    uint256 public s_tipHeight = 0;
     uint256 public s_depositNonce = 0;
 
-    mapping(uint64 => mapping(bytes32 => bool)) public unspentExitsAtBlock;
+    mapping(uint256 => mapping(bytes32 => bool)) public unspentExitsAtBlock;
     mapping(uint256 => bytes32[]) public unspentExitsNotFinalized;
+    mapping(uint256 => mapping(bytes32 => bool)) public unspentDepositsAtEthereumBlock;
 
     function computeBlockHeaderHash(BlockHeader memory header) internal pure returns (bytes32) {
         return keccak256(abi.encodePacked(header.prev, header.height, header.stateRoot, header.txRoot));
+    }
+
+    function computeDepositID(address sender, uint256 value, address color, uint256 nonce) internal pure returns (bytes32) {
+        return keccak256(abi.encodePacked(sender, value, color, nonce));
     }
 
     function computeExitTransactionID(ExitTransaction memory t) internal pure returns (bytes32) {
         return keccak256(abi.encodePacked(t.from, t.amount, t.color, t.nonce));
     }
 
-    function validateFraudProof(uint64 fraudAtHeight, bytes memory proof) internal view returns (bool) {
+    function validateFraudProof(uint256 fraudAtHeight, bytes memory proof) internal view returns (bool) {
         require(proof.length > 0);
 
-        bytes32 blockHash = s_blockHashes[fraudAtHeight];
+        bytes32 blockHash = s_commitments[fraudAtHeight].hash;
 
         // TODO implement
+
+        // Double-spending output within block
+
+        // Invalid witness for tx
+
+        // Invalid amounts in/out for tx
+
+        // Spending non-existent output
 
         return false;
     }
 
-    function postNewBlock(BlockHeader calldata header, bytes calldata txData, uint64 fraudAtHeight, bytes calldata fraudProof) external payable {
+    constructor() public {
+        // TODO genesis block hash
+        s_commitments.push(BlockCommitment(0, block.number, msg.sender, false));
+        s_tipHeight = 0;
+    }
+
+    function postNewBlock(BlockHeader calldata header, bytes calldata txData, uint256 fraudAtHeight, bytes calldata fraudProof) external payable {
         require(header.height > 0);
         require(header.height <= s_tipHeight.add(1));
-        require(header.prev == s_blockHashes[s_tipHeight]);
+        require(header.prev == s_commitments[s_tipHeight].hash);
         require(msg.value == BOND_SIZE);
-        require(s_tipEthereumHeight != block.number);
-        require(fraudAtHeight > s_tipHeight.sub(FINALIZATION_DELAY));
+        require(s_commitments[s_tipHeight].ethereumHeight != block.number);
 
         // Check fraud proof, if any
+        // Note: if fraudAtHeight is higher than tip, then maybe a separate valid fraud proof was posted before
         if (fraudAtHeight > 0 && fraudAtHeight <= s_tipHeight) {
             require(validateFraudProof(fraudAtHeight, fraudProof));
+            require(s_commitments[fraudAtHeight].isNotFinalized);
+            require(s_commitments[fraudAtHeight].ethereumHeight >= block.number.sub(FINALIZATION_DELAY));
 
             // Reset unspent exits if proof valid
-            // TODO how much gas does this use?
-            for (uint64 i = fraudAtHeight; i <= s_tipHeight; i++) {
+            // TODO how much gas does this use? maybe split it up and keep track of stale range
+            for (uint256 i = fraudAtHeight; i <= s_tipHeight; i++) {
                 bytes32[] storage unspentExits = unspentExitsNotFinalized[i];
 
                 for (uint256 j = 0; j < unspentExits.length; j++) {
@@ -85,43 +113,67 @@ contract DepositContract  {
                 unspentExits.length = 0;
             }
 
-            s_blockHashes.length = header.height;
+            s_commitments.length = header.height;
+            s_tipHeight = fraudAtHeight.sub(1);
         }
-        require(header.height == s_blockHashes.length);
+        require(header.height == s_tipHeight.add(1));
 
         // Merkleize transactions and check against tx root
+        // TODO merkleize
         // TODO add exit transactions to unspentExits
+        // TODO remove deposit spends from unspent deposits
+        // TODO check that deposits are mature (finalized)
         bytes32 txRoot;
         bytes32[] memory unspentExits;
         require(header.txRoot == txRoot);
 
         // If all checks pass, make the new block the tip
         bytes32 blockHash = computeBlockHeaderHash(header);
-        s_blockHashes.push(blockHash);
+        s_commitments.push(BlockCommitment(blockHash, block.number, msg.sender, true));
         for (uint256 i = 0; i < unspentExits.length; i++) {
             unspentExitsAtBlock[header.height][unspentExits[i]] = true;
         }
         unspentExitsNotFinalized[header.height] = unspentExits;
         s_tipHeight = header.height;
-        s_tipEthereumHeight = block.number;
+    }
+
+    function finalizeBlock(uint256 height) external {
+        require(s_commitments[height].isNotFinalized);
+        require(s_commitments[height].ethereumHeight < block.number.sub(FINALIZATION_DELAY));
+
+        address payable from = s_commitments[height].from;
+
+        delete s_commitments[height].hash;
+        delete s_commitments[height].ethereumHeight;
+        delete s_commitments[height].from;
+        s_commitments[height].isNotFinalized = false;
+
+        delete unspentExitsNotFinalized[height];
+
+        from.transfer(BOND_SIZE);
     }
 
     function deposit() external payable {
+        // TODO allow tokens
+        bytes32 depositID = computeDepositID(msg.sender, msg.value, ZERO_ADDRESS, s_depositNonce);
+        unspentDepositsAtEthereumBlock[block.number][depositID] = true;
+
         emit Deposit(msg.sender, msg.value, ZERO_ADDRESS, s_depositNonce++);
     }
 
-    function exit(uint64 blockHeight, ExitTransaction calldata exitTransaction) external {
+    function exit(uint256 height, ExitTransaction calldata exitTransaction) external {
+        // TODO remove and allow tokens
         require(exitTransaction.color == ZERO_ADDRESS);
-        require(msg.sender == exitTransaction.from);
-        require(blockHeight <= s_tipHeight.sub(FINALIZATION_DELAY));
+        require(height <= s_tipHeight);
+        require(!s_commitments[height].isNotFinalized);
 
         bytes32 exitTransactionID = computeExitTransactionID(exitTransaction);
-        require(unspentExitsAtBlock[blockHeight][exitTransactionID]);
+        require(unspentExitsAtBlock[height][exitTransactionID]);
 
         // Remove exit from unspent exits
-        unspentExitsAtBlock[blockHeight][exitTransactionID] = false;
+        unspentExitsAtBlock[height][exitTransactionID] = false;
 
         // Withdraw funds
-        msg.sender.transfer(exitTransaction.amount);
+        exitTransaction.from.transfer(exitTransaction.amount);
     }
 }
